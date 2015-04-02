336c96cc2f5d9e19ca2adab41872b007df8357b4
Evan Volgas <evan@karmakazicreative.com>
2015-04-02 14:45:34 -0400

    fix this using Exception

--- rude.py
+++ rude.py
@@ -1,17 +1,35 @@
 from random import randint
 
-rudemsg = list()
-with open ('messages.txt', 'r') as f:
-    lines = f.read().splitlines()
-    for line in lines:
-        rudemsg.append(line)
+exceptions = ["Have you done this before? Write code I mean.",
+"It's not too late to start a career in marketing",
+"Stay down, Rocky, stay down",
+"Avada Kedavra!",
+"I'm just a Python interpreter, standing in front of a developer, asking them to get their sh!t together.",
+"Your mother was a hamster and your father smelt of elderberries",
+"I'm sure it worked on your machine. Oh wait. No it didn't",
+"How about let's just stick with Hello, World for a little while longer, yeah?",
+"Right about now your boss is wishing they'd just hired monkeys and given them typewriters",
+"Have you been drinking?",
+"One day we are all gonna look back and this and laugh and have no idea you were thinking when you wrote this",
+"Just tell them the code is compiling. Who knows, it could work",
+"I know a couple of ten year olds who could help you with this",
+"Doofus",
+"If you do that one more time I'm voting you off the island",
+"And to think, someone actually pays you for this",
+"Were you by any chance dropped on your head as a child?",
+"Get off my lawn",
+"Works is better than doesn't work. Import that.",
+"Smooth. Real smooth",
+"Bet you hear a lot of village idiot jokes, don't you",
+"You code produced an error. Apparently so did your parents."]
 
-x = len(rudemsg) - 1
-y = randint(0,x)
-
-print x
-print y
-print rudemsg[y]
+y = randint(0,len(exceptions) - 1)
 
 
-#now to  write a function that overrides regular error messages and can be pip installed, etc.
+
+
+
+
+
+
+print 1 + 1

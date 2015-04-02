d851a8c656eb3fe85c1b9e9163cd217324670c2c
Evan Volgas <evan@karmakazicreative.com>
2015-04-02 15:55:38 -0400

    mods

--- rude.py
+++ rude.py
@@ -1,35 +1,44 @@
-from random import randint
+from random import choice
+ 
+INSULTS = [
+    "Have you done this before? Write code I mean.",
+    "It's not too late to start a career in marketing",
+    "Stay down, Rocky, stay down",
+    "Avada Kedavra!",
+    "I'm just a Python interpreter, standing in front of a developer, asking them to get their sh!t together.",
+    "Your mother was a hamster and your father smelt of elderberries",
+    "I'm sure it worked on your machine. Oh wait. No it didn't",
+    "How about let's just stick with Hello, World for a little while longer, yeah?",
+    "Right about now your boss is wishing they'd just hired monkeys and given them typewriters",
+    "Have you been drinking?",
+    "One day we are all gonna look back and this and laugh and have no idea you were thinking when you wrote this",
+    "Just tell them the code is compiling. Who knows, it could work",
+    "I know a couple of ten year olds who could help you with this",
+    "Doofus",
+    "If you do that one more time I'm voting you off the island",
+    "And to think, someone actually pays you for this",
+    "Were you by any chance dropped on your head as a child?",
+    "Get off my lawn",
+    "Works is better than doesn't work. Import that.",
+    "Smooth. Real smooth",
+    "Bet you hear a lot of village idiot jokes, don't you",
+    "You code produced an error. Apparently so did your parents.",
+]
+ 
+class Insult(Exception):
+    def __init__(self, exception):
+        msg = '{} {}'.format(exception.message, choice(INSULTS))
+        super(Insult, self).__init__(msg)
+ 
+ 
+def add_insult_to_injury(exception):
+    raise Insult(exception)
+ 
+ 
+#figure out a way to run this on all errors.... 
 
-exceptions = ["Have you done this before? Write code I mean.",
-"It's not too late to start a career in marketing",
-"Stay down, Rocky, stay down",
-"Avada Kedavra!",
-"I'm just a Python interpreter, standing in front of a developer, asking them to get their sh!t together.",
-"Your mother was a hamster and your father smelt of elderberries",
-"I'm sure it worked on your machine. Oh wait. No it didn't",
-"How about let's just stick with Hello, World for a little while longer, yeah?",
-"Right about now your boss is wishing they'd just hired monkeys and given them typewriters",
-"Have you been drinking?",
-"One day we are all gonna look back and this and laugh and have no idea you were thinking when you wrote this",
-"Just tell them the code is compiling. Who knows, it could work",
-"I know a couple of ten year olds who could help you with this",
-"Doofus",
-"If you do that one more time I'm voting you off the island",
-"And to think, someone actually pays you for this",
-"Were you by any chance dropped on your head as a child?",
-"Get off my lawn",
-"Works is better than doesn't work. Import that.",
-"Smooth. Real smooth",
-"Bet you hear a lot of village idiot jokes, don't you",
-"You code produced an error. Apparently so did your parents."]
-
-y = randint(0,len(exceptions) - 1)
-
-
-
-
-
-
-
-
-print 1 + 1
+if __name__ == '__main__':
+    try:
+        raise Exception('Regular exception')
+    except Exception as e:
+        add_insult_to_injury(e)
